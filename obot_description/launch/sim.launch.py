049dc62f64e22b6a1944d50d4d0d41cbf214e5b6
samuko-things <samuel.c.agba@gmail.com>
2024-04-17 08:51:07 +0100

    updated code

--- obot_description/launch/sim.launch.py
+++ obot_description/launch/sim.launch.py
@@ -2,79 +2,128 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
-
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
-from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, Command
+from launch.actions import DeclareLaunchArgument, RegisterEventHandler, EmitEvent, ExecuteProcess
 from launch_ros.actions import Node
+from launch_ros.descriptions import ParameterValue
+
+from launch.event_handlers import OnProcessExit, OnProcessStart
+from launch.events import Shutdown
+
+import xacro
+
+
+
+
+
+
+
+
+# ign service -s /world/plane_world/create --reqtype ignition.msgs.EntityFactory --reptype ignition.msgs.Boolean --timeout 1000 --req 'sdf_filename: "obot.urdf", name: "diff_bot"'
+
+
 
 
 def generate_launch_description():
 
+    # Check if we're told to use sim time
+    use_sim_time = LaunchConfiguration('use_sim_time')
 
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
-    package_name='obot_description' #<--- CHANGE ME
-    
-    world_file_name = 'empty.world'
-    world_path = os.path.join(get_package_share_directory(package_name), 'world', world_file_name)
-    
-    declare_world_cmd = DeclareLaunchArgument(
-            'world',
-            default_value=world_path,
-            description='SDF world file',
-        )
-
-    use_sim_time = 'true'
-    
-    rsp = IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                [os.path.join(get_package_share_directory(package_name),'launch','rsp.launch.py')]
-            ), 
-            launch_arguments={'use_sim_time': use_sim_time}.items()
+     # declare launch arguments
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use sim time if true'
     )
 
-    gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
+    # Path Variables 
+    pkg_path = os.path.join(get_package_share_directory('obot_description'))
 
-    # Include the Gazebo launch file, provided by the gazebo_ros package
-    gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource(
-                    [os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]
-                ),
-                launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
-             )
-
-
-    robot_name = 'obot'
-    # initial spawn position
-    x_pos = 0; y_pos = 0; z_pos = 0
-    #initial spawn orientation
-    roll = 0; pitch = 0; yaw = 0
+    ignition_ros_package = os.path.join(get_package_share_directory("ros_gz_sim"))
+    simulation_world_file = os.path.join(pkg_path, "world/empty.sdf")
     
-    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(
-        package='gazebo_ros', 
-        executable='spawn_entity.py',
-        arguments=[
-            '-topic', '/robot_description',
-            '-entity', robot_name,
-            '-x', str(x_pos), '-y', str(y_pos), '-z', str(z_pos),
-            '-R', str(roll), '-P', str(pitch), '-Y', str(yaw)
-            ],
-        output='screen')
-
     
-    # Create the launch description and populate
+    # xacro_file = os.path.join(pkg_path,'urdf','robot_urdf.xacro')
+    # robot_description_config= Command(['xacro ', xacro_file, ' use_ros2_control:=', use_ros2_control])
+    # robot_description_config= Command(['xacro ', xacro_file])
+    # robot_description_xml = ParameterValue(robot_description_config, value_type=str)
+    # params = {'robot_description': robot_description_xml, 'use_sim_time': use_sim_time}
+
+    urdf_file = os.path.join(pkg_path, 'urdf', '_robot.urdf')
+    with open(urdf_file, 'r') as infp:
+        robot_description_xml = infp.read()
+    params = {'robot_description': robot_description_xml, 'use_sim_time': use_sim_time}
+  
+    # Create a robot_state_publisher node
+    
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[params],
+    )
+
+
+    simulation = ExecuteProcess(
+        cmd=['ign', 'gazebo', '-r', simulation_world_file],
+        output='screen'
+    )
+
+    ### Can't use with event handlers
+    # simulation = IncludeLaunchDescription(
+    #                 PythonLaunchDescriptionSource(
+    #                     launch_file_path=os.join.path(ignition_ros_package, "launch/gz_sim.launch.py").as_posix()
+    #                 ),
+    #                 launch_arguments={"gz_args": "-r " + simulation_world_file}.items()
+    #             )
+
+    # urdf_file = "obot.urdf"
+    add_urdf_file = f'sdf_filename: "{urdf_file}", name: "obot"'
+    spawn_robot_in_sim_world = ExecuteProcess(
+        cmd=['ign', 'service', '-s', '/world/empty/create',
+            '--reqtype', 'ignition.msgs.EntityFactory',
+            '--reptype', 'ignition.msgs.Boolean',
+            '--timeout', '1000', '--req', add_urdf_file],
+        output='screen'
+    )
+        
+    ign_bridge_node = Node(
+        package="ros_ign_bridge",
+        executable="parameter_bridge",
+        arguments=["/model/obot/cmd_vel@geometry_msgs/msg/Twist@ignition.msgs.Twist"], 
+        # ros_arguments=["-r /model/wheeled_model/cmd_vel:=/cmd_vel"], # Remapping topic in terminal 
+        remappings=[("/model/obot/cmd_vel", "/cmd_vel")], # Remapping topic in launch file
+        output="screen"
+    )
+
+    spawn_robot_after_sim_has_started = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=simulation,
+            on_start=[spawn_robot_in_sim_world],
+        )
+    )
+    
+    simulation_exit = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=simulation,
+            on_exit=[EmitEvent(event=Shutdown())],
+        )
+    )
+
+
+     # Create the launch description and populate
     ld = LaunchDescription()
 
-
     # add the necessary declared launch arguments to the launch description
-    ld.add_action(declare_world_cmd)
+    ld.add_action(declare_use_sim_time_cmd)
     
     # Add the nodes to the launch description
-    ld.add_action(rsp)
-    ld.add_action(gazebo)
-    ld.add_action(spawn_entity)
+    ld.add_action(robot_state_publisher_node)
+    ld.add_action(simulation)
+    ld.add_action(spawn_robot_after_sim_has_started)
+    ld.add_action(ign_bridge_node)
+    ld.add_action(simulation_exit)
+
     
     return ld      # return (i.e send) the launch description for excecution
